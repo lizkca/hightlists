8259ee9f131b33084866e02ba415e59bb2d526fa
lienxiong <lizkca@163.com>
2019-07-06 07:10:05 +0800

    refactoring add func check_for_row_in_list_table

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+        
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_reward(self):
         pass
@@ -51,10 +56,7 @@ class NewVisitorTest(unittest.TestCase):
         # The table shows"1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # the page show up another frame, so that you can enter other to-do.
         # She input "Use peacock feathers to make a fly"
@@ -65,13 +67,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page update again, and shows that two to-do list.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith want to know if the website have keeped her to-do list.
         # She saw the web give a unique URL for her.
